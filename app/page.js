6c2e9c4e11551be8c25914725c43592de6ef24f1
NGFuture <78330887+NGFuture@users.noreply.github.com>
2024-06-24 21:48:59 -0700

    adds main page

--- app/page.js
+++ app/page.js
@@ -1,113 +1,182 @@
-import Image from "next/image";
+"use client";
+import { HeartFilled, HeartOutlined } from "@ant-design/icons";
+import { Pagination, Select } from "antd";
+import React, { useState, useEffect } from "react";
 
-export default function Home() {
+const API_URL = "http://localhost:8000";
+const limit = 12;
+const unknownBreed = { label: "Unknown", value: "unknown" };
+
+const Home = () => {
+  const [cats, setCats] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [favoritesFilter, setFavoritesFilter] = useState(false);
+  const [breedOptions, setBreedOptions] = useState([]);
+  const [selectedBreeds, setSelectedBreeds] = useState([]);
+
+  const toggleFavoriteFilter = () => {
+    setFavoritesFilter(!favoritesFilter);
+  };
+
+  const toggleFavorite = (id) => { 
+    let url = `${API_URL}/api/cats/${id}/favorite/`;
+    fetch(url, {
+      method: "PUT",
+    }).then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setCats(
+        cats.map((cat) =>
+          cat.id === id ? { ...cat, favorite: data.favorite } : cat
+        )
+      );
+    });
+  }
+
+  const onChange = (value, options) => {
+    setSelectedBreeds(value);
+  }
+
+
+  useEffect(() => {
+    let url = `${API_URL}/api/cats/list-cats/?limit=${limit}&skip=${
+      (page - 1) * limit
+    }`;
+    if (favoritesFilter) {
+      url += "&favorite=true";
+    }
+    if (selectedBreeds.length) {
+      url += `&breeds=${selectedBreeds.join(",")}`;
+    }
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCats(data.items);
+        setTotal(data.count);
+        setBreedOptions([unknownBreed, ...data.breeds.map((breed) => ({
+          label: breed.name,
+          value: breed.id,
+        }))]);
+      });
+  }, [page, favoritesFilter, selectedBreeds]);
+
+  useEffect(() => {
+    setPage(1);
+    }, [favoritesFilter, selectedBreeds]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex f-full overflow-hidden max-h-screen flex-col">
+        <div className="bg-gray-800 text-white flex justify-center header-part">
+          <h1 className="text-3xl font-bold">Purrfect Pix</h1>
+        </div>
+        <div className="flex flex-1 overflow-hidden main-part">
+          <div className="main-left-part ">
+            <div>
+              <button
+                className="flex items-center gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={toggleFavoriteFilter}
+              >
+                {favoritesFilter ? <HeartFilled className="text-red-500"/> : <HeartOutlined />} 
+              <span className="">Favorites only</span>
+              </button>
+            </div>
+            <div>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Breeds
+              </button>
+              <Select
+                className="select-font"
+                popupClassName="select-font"
+                mode="tags"
+                style={{
+                  width: "100%",
+                }}
+                placeholder="Choose breeds"
+                onChange={onChange}
+                options={breedOptions}
+              />
+            </div>
+            <div className="flex-1"></div>
+            <div>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Reset Database
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col flex-1">
+            <div className="p-2 flex justify-center">
+              <Pagination
+                defaultCurrent={1}
+                total={total}
+                pageSize={limit}
+                current = {page}
+                onChange={(page) => {
+                  setPage(page);
+                }}
+              />
+            </div>
+
+            <div className="overflow-y-auto p-4 flex-1">
+              {!!cats.length && (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                  {cats.map((cat) => (
+                    <div
+                      className="max-w-sm rounded overflow-hidden shadow-lg"
+                      key={cat.id}
+                    >
+                      <img className="w-full" src={cat.url} alt="Cat" />
+                      <div className="px-6 py-4">
+                        {cat.favorite ? (
+                          <HeartFilled
+                            className="text-red-500"
+                            onClick={() => {
+                              toggleFavorite(cat.id);
+                            }}
+                          />
+                        ) : (
+                          <HeartOutlined
+                            onClick={() => {
+                              toggleFavorite(cat.id);
+                            }}
+                          />
+                        )}
+                        <div className="font-bold text-xl mb-2">Cat</div>
+                        <p className="text-gray-700 text-base">{cat.url}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {!cats.length && (
+                <div className="flex justify-center">
+                  <p>No cats found</p>
+                </div>
+              )}
+            </div>
+            <div className="p-2 flex justify-center">
+              <Pagination
+                defaultCurrent={1}
+                total={total}
+                pageSize={limit}
+                current = {page}
+                onChange={(page) => {
+                  setPage(page);
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
-}
+};
+
+export default Home;
